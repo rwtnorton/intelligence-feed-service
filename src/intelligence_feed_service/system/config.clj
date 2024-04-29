4343fd9d7a9d8f20c8c90ec393cc1bec30278844
Richard W. Norton <rwtnorton@gmail.com>
2024-04-29 13:10:46 -0700

    cljfmt

--- src/intelligence_feed_service/system/config.clj
+++ src/intelligence_feed_service/system/config.clj
@@ -5,10 +5,10 @@
 (defn get-config
   ([src]
    (if-let [path (io/resource src)]
-    (-> path
-        (slurp)
-        (edn/read-string))
-    (throw (ex-info (format "bad config: %s" src)
-                    {:src src}))))
+     (-> path
+         (slurp)
+         (edn/read-string))
+     (throw (ex-info (format "bad config: %s" src)
+                     {:src src}))))
   ([]
    (get-config "config.edn")))
