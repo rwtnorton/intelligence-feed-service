b4557911903fbd0cddb5e4a756b65fe2108248de
Richard W. Norton <rwtnorton@gmail.com>
2024-04-30 18:34:31 -0700

    connect pedestal to top-level system

--- src/intelligence_feed_service/system.clj
+++ src/intelligence_feed_service/system.clj
@@ -1,6 +1,9 @@
 (ns intelligence-feed-service.system
   (:require [com.stuartsierra.component :as component]
             [intelligence-feed-service.system.config :as config]
+            [intelligence-feed-service.system.pedestal :as pedestal]
+            [intelligence-feed-service.web :as web]
+            [io.pedestal.http :as http]
             [taoensso.telemere :as logger]))
 
 (defn- resolve-config
@@ -11,16 +14,24 @@
 
 (defn- refine-config
   [cfg env]
-  (-> cfg
-      ;; Unconditonally force our given env.
-      (assoc :env env)
-      ;; Populate routes if not provided.
-      ;; (merge {::http/routes web/routes})
-      ))
+  (let [port (get-in cfg [:web :port])]
+    (-> cfg
+        ;; Unconditonally force our given env.
+        (assoc :env env)
+
+        ;; Populate routes and port if not provided.
+        (merge {::http/routes web/routes
+                ::http/port port}))))
 
 (defn- sub-systems
   [cfg env]
-  {:config (refine-config cfg env)})
+  {:config (refine-config cfg env)
+
+   :pedestal
+   (component/using (pedestal/new-pedestal)
+                    {:service-map :config
+                     ;; :repo        :repo
+                     })})
 
 (defn- down-fn
   [env sys]
